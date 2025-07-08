073d0a5f44d175cceac893fc97986b2ed2376e6a
SergiySvirkov <124861145+SergiySvirkov@users.noreply.github.com>
2025-07-08 13:30:11 +0300

    Create recipes.py

---
+++ app/api/recipes.py
@@ -0,0 +1,50 @@
+# Handles saving and retrieving recipes for authenticated users.
+
+from flask import request, jsonify
+from . import api
+from .. import db
+from ..models import Recipe, Ingredient
+from .auth import token_required
+
+@api.route('/recipes', methods=['POST'])
+@token_required
+def save_recipe(current_user):
+    """Endpoint to save a new recipe for the logged-in user."""
+    data = request.get_json()
+    if not data or not data.get('title') or not data.get('ingredients') or not data.get('instructions'):
+        return jsonify({'message': 'Missing required recipe data'}), 400
+
+    new_recipe = Recipe(
+        title=data['title'], 
+        instructions=data['instructions'], 
+        author=current_user
+    )
+
+    for ing_name in data['ingredients']:
+        ingredient = Ingredient.query.filter_by(name=ing_name).first()
+        if not ingredient:
+            ingredient = Ingredient(name=ing_name)
+            db.session.add(ingredient)
+        new_recipe.ingredients.add(ingredient)
+
+    db.session.add(new_recipe)
+    db.session.commit()
+
+    return jsonify({'message': 'Recipe saved!', 'id': new_recipe.id}), 201
+
+@api.route('/recipes', methods=['GET'])
+@token_required
+def get_recipes(current_user):
+    """Endpoint to get all recipes saved by the logged-in user."""
+    recipes = Recipe.query.filter_by(user_id=current_user.id).all()
+    output = []
+    for recipe in recipes:
+        recipe_data = {
+            'id': recipe.id,
+            'title': recipe.title,
+            'ingredients': [ing.name for ing in recipe.ingredients],
+            'instructions': recipe.instructions
+        }
+        output.append(recipe_data)
+        
+    return jsonify({'recipes': output})
