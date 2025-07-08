973d62331af541eb00e3253d5e887edc34602449
SergiySvirkov <124861145+SergiySvirkov@users.noreply.github.com>
2025-07-08 13:52:52 +0300

    Update __init__.py

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,9 +1,9 @@
 # app/api/__init__.py
-# This file ties all the API routes together.
+# We register the error handlers here.
 
 from flask import Blueprint
 
 api = Blueprint('api', __name__)
 
-# Import all the route modules to register them with the blueprint
-from . import auth, generator, recipes
+# Import routes and error handlers to register them with the blueprint
+from . import auth, generator, recipes, errors
