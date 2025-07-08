45b4a9ac9cc4a847b96e248efca18186d03ba6a2
SergiySvirkov <124861145+SergiySvirkov@users.noreply.github.com>
2025-07-08 13:19:00 +0300

    Delete # app/main directory

--- # app/main/__init__.py
+++
@@ -1,9 +0,0 @@
-# app/main/__init__.py
-
-from flask import Blueprint
-
-# This blueprint will handle non-API routes, like a simple health check.
-main = Blueprint('main', __name__)
-
-# Import routes at the end to avoid circular dependencies
-from . import routes
